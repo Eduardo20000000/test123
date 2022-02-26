3bbc577a4ce85f867f388670732c959328bc7d5d
Eduardo20000000 <96834976+Eduardo20000000@users.noreply.github.com>
2022-02-25 21:52:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-let prueba = true
-let 12 = "eeee"
-let 1 = "Noob!"
+let pelota = "eeee"
+let nada = "Noob!"
 console.log(12)
 console.log(1)
+console.log(its just a websoket / simplemente a websoket xD)
