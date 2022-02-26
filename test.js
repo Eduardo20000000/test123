6735d8599f77be59c2f4031c5536c74523ca63fd
Eduardo20000000 <96834976+Eduardo20000000@users.noreply.github.com>
2022-02-25 21:36:08 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,5 @@
 let prueba = true
 let 12 = "eeee"
-let alert = "Noob!"
-alert (alert)
+let 1 = "Noob!"
 console.log(12)
-if(prueba == true) {
-  alert ()
-  else
-    alert(12)
-}
-  
+console.log(1)
